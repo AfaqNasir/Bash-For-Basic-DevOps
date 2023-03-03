6cbed1b508dcb380ccd3f3f2cf01409900e45225
Muhammad Afaq Nasir <48288031+AfaqNasir@users.noreply.github.com>
2023-03-03 16:51:12 +0500

    This File is to install docker in Ubuntu
    ## This File is to install docker in Ubuntu ##
    
    # apt update
    
    # apt upgrade
    
    # install docker
    
    # start docker service
    
    # enable the docker service to continue the docker
    
    #check the docker version

---
+++ DockerInstall.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+## This File is to install docker in ubuntu ##
+
+# apt update
+sudo apt-get update
+
+# apt upgrade
+sudo apt-get upgrade
+
+# install docker
+sudo apt install docker.io -y
+
+# start docker service
+systemctl start docker
+
+# enable docker service to continue the docker
+systemctl enable docker
+
+#check the docker version
+docker --version
